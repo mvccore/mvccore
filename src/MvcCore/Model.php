a5109be1dc7d0de30367a137385ed33ce38c5375
Tom Flídr <tomflidr@gmail.com>
2017-01-22 06:43:26 +0100

    most protected methods changed to public, refactoring in GetValues(), SetUp(), fixed GetDb(), added __call(), added mysql pdo option PDO::ATTR_EMULATE_PREPARES => FALSE

--- src/MvcCore/Model.php
+++ src/MvcCore/Model.php
@@ -13,8 +13,7 @@
 
 require_once('Config.php');
 
-abstract class MvcCore_Model
-{
+abstract class MvcCore_Model {
 	/**
 	 * PDO connection arguments.
 	 * 
@@ -66,6 +65,7 @@ abstract class MvcCore_Model
 			'auth'		=> TRUE,
 			'fileDb'	=> FALSE,
 			'options'	=> array(
+				'PDO::ATTR_EMULATE_PREPARES'		=> FALSE, // let params inserting on database
 				'PDO::MYSQL_ATTR_MULTI_STATEMENTS'	=> TRUE,
 				'PDO::MYSQL_ATTR_INIT_COMMAND'		=> "SET NAMES 'UTF8'",
 			),
@@ -158,7 +158,7 @@ abstract class MvcCore_Model
 
 	/**
 	 * Creates an instance and inits cfg, db and resource properties
-	 * @param mixed $connectionIndex 
+	 * @param int $connectionIndex 
 	 */
 	public function __construct ($connectionIndex = -1) {
 		if ($this->autoInit) $this->Init($connectionIndex);
@@ -166,29 +166,33 @@ abstract class MvcCore_Model
 
 	/**
 	 * Creates an instance and inits cfg, db and resource properties
-	 * @param mixed $connectionIndex
+	 * @param int $connectionIndex
 	 */
 	public function Init ($connectionIndex = -1) {
 		if ($connectionIndex == -1) $connectionIndex = static::$connectionIndex;
-		$this->cfg = static::getCfg($connectionIndex);
-		$this->db = static::getDb($connectionIndex);
+		$this->cfg = static::GetCfg($connectionIndex);
+		$this->db = static::GetDb($connectionIndex);
 		$this->resource = static::getResource(array(), get_class($this));
 	}
 
 	/**
 	 * Collect all model class public and inherit field values into array
+	 * @param boolean $getNullValues			if true, include also values with NULLs, by default - FALSE
 	 * @param boolean $includeInheritProperties if true, include only fields from current model class and from parent classes
 	 * @param boolean $publicOnly               if true, include only public model fields
 	 * @return array
 	 */
-	protected function getValues ($includeInheritProperties = TRUE, $publicOnly = TRUE) {
+	public function GetValues ($getNullValues = FALSE, $includeInheritProperties = TRUE, $publicOnly = TRUE) {
 		$data = array();
+		$systemProperties = array('autoInit' => 1, 'db' => 1, 'cfg' => 1, 'resource' => 1);
 		$modelClassName = get_class($this);
 		$classReflector = new ReflectionClass($modelClassName);
 		$properties = $publicOnly ? $classReflector->getProperties(ReflectionProperty::IS_PUBLIC) : $classReflector->getProperties();
 		foreach ($properties as $property) {
 			if (!$includeInheritProperties && $property->class != $modelClassName) continue;
 			$propertyName = $property->name;
+			if (isset($systemProperties[$propertyName])) continue;
+			if (!$getNullValues && is_null($this->$propertyName)) continue;
 			$data[$propertyName] = $this->$propertyName;
 		}
 		return $data;
@@ -197,26 +201,38 @@ abstract class MvcCore_Model
 	/**
 	 * Set up given $data items into $this instance context 
 	 * as typed properties by PHP doc comments, as properties
-	 * with the same names as $data array keys - case sensitive.
+	 * with the same names as $data array keys - case sesitively by default.
+	 * Do not set any $data items which are not declared in $this context.
 	 * @param array   $data                     collection with data to set up
+	 * @param boolean $keysInsensitive			if true, set up properties from $data with case insensivity
 	 * @param boolean $includeInheritProperties if true, include only fields from current model class and from parent classes
 	 * @param boolean $publicOnly               if true, include only public model fields
 	 * @return MvcCore_Model
 	 */
-	protected function setUp ($data = array(), $includeInheritProperties = TRUE, $publicOnly = TRUE) {
+	public function SetUp ($data = array(), $keysInsensitive = FALSE, $includeInheritProperties = TRUE, $publicOnly = TRUE) {
 		$modelClassName = get_class($this);
 		$classReflector = new ReflectionClass($modelClassName);
 		$properties = $publicOnly ? $classReflector->getProperties(ReflectionProperty::IS_PUBLIC) : $classReflector->getProperties();
+		$dataKeys = $keysInsensitive ? ','.implode(',', array_keys($data)).',' : '' ;
 		foreach ($properties as $property) {
 			if (!$includeInheritProperties && $property->class != $modelClassName) continue;
 			$propertyName = $property->name;
+			if (isset($data[$propertyName])) {
+				$value = $data[$propertyName];
+			} else if ($keysInsensitive) {
+				// try to search with not case sensitively same property name
+				$dataKeyPos = stripos($dataKeys, ','.$propertyName.',');
+				if ($dataKeyPos === FALSE) continue;
+				$dataKey = substr($dataKeys, $dataKeyPos + 1, strlen($propertyName));
+				$value = $data[$dataKey];
+			} else {
+				continue;
+			}
 			if (preg_match('/@var\s+([^\s]+)/', $property->getDocComment(), $matches)) {
 				list(, $type) = $matches;
-				settype($data[$propertyName], $type);
+				settype($value, $type);
 			}
-			if (isset($data[$propertyName])) {
-				$this->$propertyName =  $data[$propertyName];
-			}
+			$this->$propertyName = $value;
 		}
 		return $this;
 	}
@@ -224,7 +240,7 @@ abstract class MvcCore_Model
 	/**
 	 * Returns (or creates and holds) instance from local store
 	 * @param mixed $arg,... unlimited OPTIONAL variables to pass into __construct() method
-	 * @return App_Models_Base|mixed
+	 * @return MvcCore_Model|mixed
 	 */
 	public static function GetInstance (/* $arg1, $arg2, $arg, ... */) {
 		// get 'ClassName' string from this call: ClassName::GetInstance();
@@ -245,18 +261,18 @@ abstract class MvcCore_Model
 	 * @param int $connectionIndex 
 	 * @return PDO
 	 */
-	protected static function getDb ($connectionIndex = -1) {
+	public static function GetDb ($connectionIndex = -1) {
 		if ($connectionIndex == -1) $connectionIndex = static::$connectionIndex;
 		if (!isset(static::$connections[$connectionIndex])) {
 			// get system config 'db' data 
 			// and get predefined constructor arguments by driver value from config
-			$cfg = static::getCfg($connectionIndex);
+			$cfg = static::GetCfg($connectionIndex);
 			$conArgs = (object) self::$connectionArguments[isset(self::$connectionArguments[$cfg->driver]) ? $cfg->driver:'default'];
 			$connection = NULL;
 			// If database is filesystem based, complete app root and extend
 			// relative path in $cfg->dbname to absolute path
 			if ($conArgs->fileDb) {
-				$appRoot = MvcCore::GetRequest()->appRoot;
+				$appRoot = MvcCore::GetInstance()->GetRequest()->AppRoot;
 				if (strpos($appRoot, 'phar://') !== FALSE) {
 					$lastSlashPos = strrpos($appRoot, '/');
 					$appRoot = substr($appRoot, 7, $lastSlashPos - 7);
@@ -284,7 +300,7 @@ abstract class MvcCore_Model
 	 * @param int $connectionIndex 
 	 * @return object
 	 */
-	protected static function getCfg ($connectionIndex = -1) {
+	public static function GetCfg ($connectionIndex = -1) {
 		if ($connectionIndex == -1) $connectionIndex = static::$connectionIndex;
 		if (!isset(static::$configs[$connectionIndex])) {
 			$cfg = MvcCore_Config::GetSystem();
@@ -304,7 +320,7 @@ abstract class MvcCore_Model
 	 * @param string $resourceClassPath
 	 * @return App_Models_Base(_Resource)
 	 */
-	protected static function getResource ($args = array(), $modelClassName = '', $resourceClassPath = '_Resource') {
+	public static function GetResource ($args = array(), $modelClassName = '', $resourceClassPath = '_Resource') {
 		$result = NULL;
 		if (!$modelClassName) $modelClassName = get_called_class();
 		// do not create resource instance in resource class (if current class name doesn't end with '_Resource' substring):
@@ -318,10 +334,46 @@ abstract class MvcCore_Model
 		return $result;
 	}
 
+	/**
+	 * Sets any custom property ('PropertyName') by $model->SetPropertyName('value'),
+	 * which is not necessary to define previously or gets previously defined 
+	 * property ('PropertyName') by $model->GetPropertyName(); Throws exception 
+	 * if no property defined by get call or if virtual call begins with anything 
+	 * different from 'set' or 'get'.
+	 * This method returns custom value for get and $model instance for set.
+	 * @param string $rawName 
+	 * @param array  $arguments 
+	 * @throws Exception 
+	 * @return mixed|MvcCore_Model
+	 */
+	public function __call ($rawName, $arguments = array()) {
+		$nameBegin = strtolower(substr($rawName, 0, 3));
+		$name = substr($rawName, 3);
+		if ($nameBegin == 'get' && isset($this->$name)) {
+			return $this->$name;
+		} else if ($nameBegin == 'set') {
+			$this->$name = isset($arguments[0]) ? $arguments[0] : NULL;
+			return $this;
+		} else {
+			throw new Exception('['.__CLASS__."] No property with name '$name' defined.");
+		}
+	}
+
+	/**
+	 * Set any custom property, not necessary to previously define.
+	 * @param string $name 
+	 * @param mixed  $value 
+	 */
 	public function __set ($name, $value) {
 		$this->$name = $value;
 	}
 
+	/**
+	 * Get any custom property, not necessary to previously define,
+	 * if property is not defined, NULL is returned.
+	 * @param string $name 
+	 * @return mixed
+	 */
 	public function __get ($name) {
 		return (isset($this->$name)) ? $this->$name : null;
 	}
