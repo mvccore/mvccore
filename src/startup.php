bbedf119ebf55ea83fa0e1ea2b5e65398d1b27ef
Tom Flídr <tomflidr@gmail.com>
2021-03-14 18:22:14 +0100

    Complete refactoing, added constants for closured variables.

--- src/startup.php
+++ src/startup.php
@@ -12,29 +12,44 @@
  */
 
 call_user_func(function () {
-	error_reporting(E_ALL);
-	if (\PHP_VERSION_ID < 50400)
-		die("MvcCore requires at least PHP version 5.4.0, your PHP version is: " . PHP_VERSION . ".");
-	$iniShirtOpenTags = @ini_get("short_open_tag");
-	if ($iniShirtOpenTags === FALSE || strtolower($iniShirtOpenTags) == 'off' || $iniShirtOpenTags == '0')
-		die("MvcCore templates require PHP short open tags enabled. Set `short_open_tag = On` in your php.ini.");
-	if (\PHP_SAPI == 'cli') {
-		$backtraceItems = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
-		$scriptFilename =  $backtraceItems[count($backtraceItems) - 1]['file'];
-	} else {
-		$scriptFilename = $_SERVER['SCRIPT_FILENAME'];
+	static $includePaths = NULL;
+	static $currentDir = NULL;
+	if (!defined('MVCCORE_REQUIREMENTS')) {
+		if (\PHP_VERSION_ID < 50400)
+			die("MvcCore requires at least PHP version 5.4.0, your PHP version is: " . PHP_VERSION . ".");
+		define('MVCCORE_REQUIREMENTS', TRUE);
 	}
-	$scriptFilename = str_replace('\\', '/', $scriptFilename);
-	$appRootPath = strpos(__FILE__, 'phar://') === 0
-		? 'phar://' . $scriptFilename
-		: dirname($scriptFilename);
-	$includePaths = [
-		$appRootPath,
-		$appRootPath . '/App',
-		$appRootPath . '/Libs',
-	];
-	$currentDir = str_replace('\\', '/', __DIR__);
-	if (!in_array($currentDir, $includePaths, TRUE)) array_unshift($includePaths, $currentDir);
+	if (!defined('MVCCORE_DOCUMENT_ROOT')) {
+		if (\PHP_SAPI === 'cli') {
+			$backtraceItems = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 1);
+			$scriptFilename = $backtraceItems[count($backtraceItems) - 1]['file'];
+		} else {
+			$scriptFilename = $_SERVER['SCRIPT_FILENAME'];
+		}
+		// `ucfirst()` - cause IIS has lower case drive name here - different from __DIR__ value
+		$scriptFilename = ucfirst(str_replace(['\\', '//'], '/', $scriptFilename));
+		define('MVCCORE_DOCUMENT_ROOT', strlen(\Phar::running()) > 0 
+			? 'phar://' . $scriptFilename
+			: dirname($scriptFilename)
+		);
+	}
+	if (!defined('MVCCORE_APP_ROOT')) 
+		define('MVCCORE_APP_ROOT', constant('MVCCORE_DOCUMENT_ROOT'));
+	if ($includePaths === NULL) {
+		if (defined('MVCCORE_INCLUDE_PATHS')) {
+			$includePaths = explode(PATH_SEPARATOR, constant('MVCCORE_INCLUDE_PATHS'));
+		} else {
+			$includePaths = [
+				MVCCORE_APP_ROOT,
+				MVCCORE_APP_ROOT . '/App',
+				MVCCORE_APP_ROOT . '/Libs',
+			];
+		}
+	}
+	if ($currentDir === NULL) 
+		$currentDir = str_replace('\\', '/', __DIR__);
+	if (!in_array($currentDir, $includePaths, TRUE)) 
+		array_unshift($includePaths, $currentDir);
 	$autoload = function ($className) use ($includePaths) {
 		$classSeparator = mb_strpos($className, '\\') === FALSE ? '_' : '\\';
 		$fileName = str_replace($classSeparator, '/', $className) . '.php';
