6011c580cf11563a4275249f5dd8d8860bd548bf
Tom Flídr <tomflidr@gmail.com>
2024-11-20 13:38:52 +0100

    Application - setters and getters for new path properties.

--- src/MvcCore/Application/GettersSetters.php
+++ src/MvcCore/Application/GettersSetters.php
@@ -44,6 +44,7 @@ trait GettersSetters {
 	 * @return string
 	 */
 	public function GetCompiled () {
+		/** @var \MvcCore\Application $this */
 		if ($this->compiled === NULL) {
 			$compiled = static::NOT_COMPILED;
 			if (class_exists('\Phar') && strlen(\Phar::running()) > 0) {
@@ -233,50 +234,255 @@ trait GettersSetters {
 	 * @inheritDoc
 	 * @return string
 	 */
-	public function GetAppDir () {
-		if ($this->appDir === NULL) {
-			if (!defined('MVCCORE_APP_ROOT_DIRNAME')) 
-				define('MVCCORE_APP_ROOT_DIRNAME', 'App');
-			$this->appDir = constant('MVCCORE_APP_ROOT_DIRNAME');
+	public function GetControllersBaseNamespace () {
+		if ($this->controllersBaseNamespace === NULL) {
+			$appRootFp = $this->GetPathAppRoot();
+			$ctrlsFp = $this->GetPathControllers(TRUE);
+			if (mb_strpos($ctrlsFp, $appRootFp) === 0) {
+				// controllers are inside app root
+				$ctrlsPath = mb_substr($ctrlsFp, mb_strlen($appRootFp));
+				$this->controllersBaseNamespace = str_replace('/', "\\", $ctrlsPath);
+			} else {
+				// controllers are outside app root
+				$this->controllersBaseNamespace = str_replace('/', "\\", ltrim($this->GetPathControllers(FALSE), '~/'));
+			}
 		}
-		return $this->appDir;
+		return $this->controllersBaseNamespace;
 	}
-	
+
 	/**
 	 * @inheritDoc
+	 * @param  string $pathName
+	 * @param  bool   $absolute
+	 * @param  bool   $public
 	 * @return string
 	 */
-	public function GetDocRootDir () {
-		if ($this->docRootDir === NULL) {
-			if (!defined('MVCCORE_DOC_ROOT_DIRNAME')) 
-				define('MVCCORE_DOC_ROOT_DIRNAME', 'www');
-			$this->docRootDir = constant('MVCCORE_DOC_ROOT_DIRNAME');
+	public function GetPath ($pathName, $absolute = FALSE, $public = FALSE) {
+		if (!isset($this->paths[$pathName])) {
+			$this->SetPath($pathName, $this->{$pathName}, $public);
 		}
-		return $this->docRootDir;
-	}
-	
-	/**
-	 * @inheritDoc
-	 * @return string
-	 */
-	public function GetCliDir () {
-		return $this->cliDir;
+		return $this->paths[$pathName][$absolute ? 1 : 0];
 	}
 
 	/**
 	 * @inheritDoc
 	 * @return string
 	 */
-	public function GetControllersDir () {
-		return $this->controllersDir;
+	public function GetPathAppRoot () {
+		if ($this->pathAppRoot === NULL) {
+			if (defined('MVCCORE_APP_ROOT')) {
+				$this->pathAppRoot = constant('MVCCORE_APP_ROOT');
+				$insidePhar = class_exists('\Phar') && strlen(\Phar::running()) > 0;
+				if (!$insidePhar)
+					$this->pathAppRoot = ucfirst($this->pathAppRoot);
+			} else {
+				$docRoot = $this->GetPathDocRoot();
+				$docRootDirName = static::getPathDocRootDirName();
+				$docRootDirNamePos = mb_strrpos($docRoot, '/' . $docRootDirName);
+				$estimatedPos = mb_strlen($docRoot) - mb_strlen($docRootDirName) - 1;
+				$this->pathAppRoot = $docRootDirNamePos !== FALSE && $docRootDirNamePos === $estimatedPos
+					? mb_substr($docRoot, 0, $estimatedPos)
+					: $docRoot;
+				define('MVCCORE_APP_ROOT', $this->pathAppRoot);
+			}
+		}
+		return $this->pathAppRoot;
 	}
 
 	/**
 	 * @inheritDoc
+	 * @return string|NULL
+	 */
+	public function GetPathAppRootVendor () {
+		if ($this->vendorAppDispatch === NULL)
+			$this->initVendorProps();
+		return $this->pathAppRootVendor;
+	}
+	
+	/**
+	 * @inheritDoc
 	 * @return string
 	 */
-	public function GetViewsDir () {
-		return $this->viewsDir;
+	public function GetPathDocRoot () {
+		if ($this->pathDocRoot === NULL) {
+			// `ucfirst()` - cause IIS has lower case drive name here - different from __DIR__ value
+			$insidePhar = class_exists('\Phar') && strlen(\Phar::running()) > 0;
+			if (defined('MVCCORE_DOC_ROOT')) {
+				$this->pathDocRoot = constant('MVCCORE_DOC_ROOT');
+				if (!$insidePhar)
+					$this->pathDocRoot = ucfirst($this->pathDocRoot);
+			} else {
+				$globalServer = $this->GetRequest()->GetGlobalCollection('server');
+				if (php_sapi_name() !== 'cli') {
+					// running in web server:
+					$scriptFilename = $globalServer['SCRIPT_FILENAME'];
+				} else {
+					// running in CLI:
+					$backtraceItems = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+					$scriptFilename = $backtraceItems[count($backtraceItems) - 1]['file'];
+					// If php is running by direct input like `php -r "/* php code */":
+					if (
+						mb_strpos($scriptFilename, DIRECTORY_SEPARATOR) === FALSE && // $indexFilePath = "Command line code"
+						empty($globalServer['SCRIPT_FILENAME'])
+					) {
+						// Try to define app root and document root 
+						// by possible Composer class location:
+						$composerFullClassName = 'Composer\\Autoload\\ClassLoader';
+						if (class_exists($composerFullClassName, TRUE)) {
+							$ccType = new \ReflectionClass($composerFullClassName);
+							$scriptFilename = dirname($ccType->getFileName(), 2);
+						} else {
+							// If there is no composer class, define 
+							// document root by called current working directory:
+							$scriptFilename = getcwd() . '/php';
+						}
+					}
+				}
+				$docRoot = $insidePhar
+					? $scriptFilename
+					: dirname($scriptFilename);
+				// `ucfirst()` - cause IIS has lower case drive name here - different from __DIR__ value
+				$docRoot = str_replace(['\\', '//'], '/', ucfirst($docRoot));
+				$this->pathDocRoot = $insidePhar 
+					? 'phar://' . $docRoot 
+					: $docRoot;
+				define('MVCCORE_DOC_ROOT', $this->pathDocRoot);
+			}
+		}
+		return $this->pathDocRoot;
+	}
+
+	/**
+	 * @inheritDoc
+	 * @param  bool $absolute
+	 * @return string
+	 */
+	public function GetPathApp ($absolute = FALSE) {
+		if (!isset($this->paths['pathApp'])) {
+			if (!isset($this->pathApp)) {
+				$appRootDirName = static::getPathAppRootDirName();
+				$this->pathApp = '~/' . $appRootDirName;
+				$absPath = $this->pathAppRoot . '/' . $appRootDirName;
+			} else {
+				if (mb_substr($this->pathApp, 0, 2) === '~/') {
+					$absPath = $this->pathAppRoot . mb_substr($this->pathApp, 1);
+				} else {
+					$absPath = $this->pathApp;
+				}
+			}
+			$this->paths['pathApp'] = [$this->pathApp, $absPath];
+		}
+		return $this->paths['pathApp'][$absolute ? 1 : 0];
+	}
+	
+	/**
+	 * @inheritDoc
+	 * @param  bool $absolute
+	 * @return string
+	 */
+	public function GetPathCli ($absolute = FALSE) {
+		return $this->GetPath('pathCli', $absolute, FALSE);
+	}
+	
+	/**
+	 * @inheritDoc
+	 * @param  bool $absolute
+	 * @return string
+	 */
+	public function GetPathControllers ($absolute = FALSE) {
+		return $this->GetPath('pathControllers', $absolute, FALSE);
+	}
+	
+	/**
+	 * @inheritDoc
+	 * @param  bool $absolute
+	 * @return string
+	 */
+	public function GetPathViews ($absolute = FALSE) {
+		return $this->GetPath('pathViews', $absolute, FALSE);
+	}
+	
+	/**
+	 * @inheritDoc
+	 * @param  bool $absolute
+	 * @return string
+	 */
+	public function GetPathViewHelpers ($absolute = FALSE) {
+		return $this->GetPath('pathViewHelpers', $absolute, FALSE);
+	}
+	
+	/**
+	 * @inheritDoc
+	 * @param  bool $absolute
+	 * @return string
+	 */
+	public function GetPathViewLayouts ($absolute = FALSE) {
+		return $this->GetPath('pathViewLayouts', $absolute, FALSE);
+	}
+	
+	/**
+	 * @inheritDoc
+	 * @param  bool $absolute
+	 * @return string
+	 */
+	public function GetPathViewScripts ($absolute = FALSE) {
+		return $this->GetPath('pathViewScripts', $absolute, FALSE);
+	}
+	
+	/**
+	 * @inheritDoc
+	 * @param  bool $absolute
+	 * @return string
+	 */
+	public function GetPathViewForms ($absolute = FALSE) {
+		return $this->GetPath('pathViewForms', $absolute, FALSE);
+	}
+	
+	/**
+	 * @inheritDoc
+	 * @param  bool $absolute
+	 * @return string
+	 */
+	public function GetPathViewFormsFields ($absolute = FALSE) {
+		return $this->GetPath('pathViewFormsFields', $absolute, FALSE);
+	}
+
+	// forms, fields, 
+	
+	/**
+	 * @inheritDoc
+	 * @param  bool $absolute
+	 * @return string
+	 */
+	public function GetPathVar ($absolute = FALSE) {
+		return $this->GetPath('pathVar', $absolute, FALSE);
+	}
+	
+	/**
+	 * @inheritDoc
+	 * @param  bool $absolute
+	 * @return string
+	 */
+	public function GetPathTmp ($absolute = FALSE) {
+		return $this->GetPath('pathTmp', $absolute, FALSE);
+	}
+	
+	/**
+	 * @inheritDoc
+	 * @param  bool $absolute
+	 * @return string
+	 */
+	public function GetPathLogs ($absolute = FALSE) {
+		return $this->GetPath('pathLogs', $absolute, FALSE);
+	}
+	
+	/**
+	 * @inheritDoc
+	 * @param  bool $absolute
+	 * @return string
+	 */
+	public function GetPathStatic ($absolute = FALSE) {
+		return $this->GetPath('pathStatic', $absolute, TRUE);
 	}
 
 	/**
@@ -317,6 +523,30 @@ trait GettersSetters {
 	 */
 	public function GetTerminated () {
 		return $this->terminated;
+	}
+	
+	/**
+	 * Internal getter to init or define constant 
+	 * `MVCCORE_APP_ROOT_DIRNAME`, which always contains 
+	 * main application directory name `"App"`.
+	 * @return string
+	 */
+	protected static function getPathAppRootDirName () {
+		if (!defined('MVCCORE_APP_ROOT_DIRNAME')) 
+			define('MVCCORE_APP_ROOT_DIRNAME', 'App');
+		return MVCCORE_APP_ROOT_DIRNAME;
+	}
+	
+	/**
+	 * Internal getter to init or define constant 
+	 * `MVCCORE_DOC_ROOT_DIRNAME`, which always contains 
+	 * document root name `"www"`.
+	 * @return string
+	 */
+	protected static function getPathDocRootDirName () {
+		if (!defined('MVCCORE_DOC_ROOT_DIRNAME')) 
+			define('MVCCORE_DOC_ROOT_DIRNAME', 'www');
+		return MVCCORE_DOC_ROOT_DIRNAME;
 	}
 
 
@@ -465,54 +695,199 @@ trait GettersSetters {
 		return $this;
 	}
 
-
 	/**
 	 * @inheritDoc
-	 * @param  string $appDir
-	 * @return \MvcCore\Application
+	 * @param  string $controllersBaseNamespace
+	 * @return string
 	 */
-	public function SetAppDir ($appDir) {
-		$this->appDir = $appDir;
+	public function SetControllersBaseNamespace ($controllersBaseNamespace) {
+		$this->controllersBaseNamespace = $controllersBaseNamespace;
 		return $this;
 	}
 
 	/**
 	 * @inheritDoc
-	 * @param  string $docRootDir
+	 * @param  string $pathName
+	 * @param  string $relPath
+	 * @param  bool   $public
 	 * @return \MvcCore\Application
 	 */
-	public function SetDocRootDir ($docRootDir) {
-		$this->docRootDir = $docRootDir;
+	public function SetPath ($pathName, $relPath, $public = FALSE) {
+		if (mb_substr($relPath, 0, 2) === '~/') {
+			$rootDir = $public 
+				? $this->GetPathDocRoot()
+				: $this->GetPathAppRoot();
+			$toolClass = $this->toolClass;
+			$absPath = $toolClass::RealPathVirtual($rootDir . mb_substr($relPath, 1));
+		} else {
+			$absPath = $relPath;
+		}
+		$this->paths[$pathName] = [$relPath, $absPath];
 		return $this;
 	}
 	
 	/**
 	 * @inheritDoc
-	 * @param  string $cliDir
+	 * @param  string $absPath
 	 * @return \MvcCore\Application
 	 */
-	public function SetCliDir ($cliDir) {
-		$this->cliDir = $cliDir;
+	public function SetPathAppRoot ($absPath) {
+		$this->pathAppRoot = $absPath;
+		return $this;
+	}
+	
+	/**
+	 * @inheritDoc
+	 * @param  string $absPath
+	 * @return \MvcCore\Application
+	 */
+	public function SetPathAppRootVendor ($absPath) {
+		if ($this->vendorAppDispatch === NULL)
+			$this->initVendorProps();
+		if ($this->vendorAppRoot) {
+			$this->pathAppRootVendor = $absPath;
+		}
+		return $this;
+	}
+	
+	/**
+	 * @inheritDoc
+	 * @param  string $absPath
+	 * @return \MvcCore\Application
+	 */
+	public function SetPathDocRoot ($absPath) {
+		$this->pathDocRoot = $absPath;
+		return $this;
+	}
+	
+	/**
+	 * @inheritDoc
+	 * @param  string $relPath
+	 * @return \MvcCore\Application
+	 */
+	public function SetPathApp ($relPath) {
+		$this->pathApp = $relPath;
+		$this->SetPath('pathApp', $relPath, FALSE);
+		return $this;
+	}
+	
+	/**
+	 * @inheritDoc
+	 * @param  string $relPath
+	 * @return \MvcCore\Application
+	 */
+	public function SetPathCli ($relPath) {
+		$this->SetPath('pathClis', $relPath, FALSE);
+		return $this;
+	}
+	
+	/**
+	 * @inheritDoc
+	 * @param  string $relPath
+	 * @return \MvcCore\Application
+	 */
+	public function SetPathControllers ($relPath) {
+		$this->SetPath('pathControllers', $relPath, FALSE);
 		return $this;
 	}
 
 	/**
 	 * @inheritDoc
-	 * @param  string $controllersDir
+	 * @param  string $relPath
 	 * @return \MvcCore\Application
 	 */
-	public function SetControllersDir ($controllersDir) {
-		$this->controllersDir = $controllersDir;
+	public function SetPathViews ($relPath) {
+		$this->SetPath('pathViews', $relPath, FALSE);
+		return $this;
+	}
+	
+	/**
+	 * @inheritDoc
+	 * @param  string $relPath
+	 * @return \MvcCore\Application
+	 */
+	public function SetPathViewHelpers ($relPath) {
+		$this->SetPath('pathViewHelpers', $relPath, FALSE);
 		return $this;
 	}
 
 	/**
 	 * @inheritDoc
-	 * @param  string $viewsDir
+	 * @param  string $relPath
 	 * @return \MvcCore\Application
 	 */
-	public function SetViewsDir ($viewsDir) {
-		$this->viewsDir = $viewsDir;
+	public function SetPathViewLayouts ($relPath) {
+		$this->SetPath('pathViewLayouts', $relPath, FALSE);
+		return $this;
+	}
+
+	/**
+	 * @inheritDoc
+	 * @param  string $relPath
+	 * @return \MvcCore\Application
+	 */
+	public function SetPathViewScripts ($relPath) {
+		$this->SetPath('pathViewScripts', $relPath, FALSE);
+		return $this;
+	}
+
+	/**
+	 * @inheritDoc
+	 * @param  string $relPath
+	 * @return \MvcCore\Application
+	 */
+	public function SetPathViewForms ($relPath) {
+		$this->SetPath('pathViewForms', $relPath, FALSE);
+		return $this;
+	}
+
+	/**
+	 * @inheritDoc
+	 * @param  string $relPath
+	 * @return \MvcCore\Application
+	 */
+	public function SetPathViewFormsFields ($relPath) {
+		$this->SetPath('pathViewFormsFields', $relPath, FALSE);
+		return $this;
+	}
+
+	/**
+	 * @inheritDoc
+	 * @param  string $relPath
+	 * @return \MvcCore\Application
+	 */
+	public function SetPathVar ($relPath) {
+		$this->SetPath('pathVar', $relPath, FALSE);
+		return $this;
+	}
+	
+	/**
+	 * @inheritDoc
+	 * @param  string $relPath
+	 * @return \MvcCore\Application
+	 */
+	public function SetPathTmp ($relPath) {
+		$this->SetPath('pathTmp', $relPath, FALSE);
+		return $this;
+	}
+	
+	/**
+	 * @inheritDoc
+	 * @param  string $relPath
+	 * @return \MvcCore\Application
+	 */
+	public function SetPathLogs ($relPath) {
+		$this->SetPath('pathLogs', $relPath, FALSE);
+		return $this;
+	}
+	
+	/**
+	 * @inheritDoc
+	 * @param  string $relPath
+	 * @return \MvcCore\Application
+	 */
+	public function SetPathStatic ($relPath) {
+		$this->SetPath('pathStatic', $relPath, TRUE);
 		return $this;
 	}
 
