471cdc27cad6f3c0eb266a06993ef0a904ee4ea2
Tom Flídr <tomflidr@gmail.com>
2025-01-10 14:21:14 +0100

    release v5.3.5

--- src/MvcCore/Application.php
+++ src/MvcCore/Application.php
@@ -23,7 +23,7 @@ class Application implements \MvcCore\IApplication {
 	 * Comparison by PHP function `version_compare();`.
 	 * @see http://php.net/manual/en/function.version-compare.php
 	 */
-	const VERSION = '5.3.4';
+	const VERSION = '5.3.5';
 
 	/**
 	 * Include traits with
